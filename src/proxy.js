9ebf94637355cabd54305ba2fc20015ab69498a4
Rushs321 <fyggh566@tutamail.com>
2024-08-28 17:48:21 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,19 +1,51 @@
 const axios = require('axios');
 const pick = require('lodash').pick;
+const { generateRandomIP, randomUserAgent } = require('./user.js');
 const shouldCompress = require('./shouldCompress');
 const redirect = require('./redirect');
 const compress = require('./compress');
 const copyHeaders = require('./copyHeaders');
 
+const viaHeaders = [
+    '1.1 example-proxy-service.com (ExampleProxy/1.0)',
+    '1.0 another-proxy.net (Proxy/2.0)',
+    '1.1 different-proxy-system.org (DifferentProxy/3.1)',
+    '1.1 some-proxy.com (GenericProxy/4.0)',
+];
+
+function randomVia() {
+    const index = Math.floor(Math.random() * viaHeaders.length);
+    return viaHeaders[index];
+}
+
+
 async function proxy(req, res) {
+
+  const { url, jpeg, bw, l } = req.query;
+  if (!url) {
+
+        return res.end(`1we23`);
+  }
+  
+  const urls = Array.isArray(url) ? url.join('&url=') : url;
+  const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
+
+  req.params.url = cleanedUrl;
+  req.params.webp = !jpeg;
+  req.params.grayscale = bw !== '0';
+  req.params.quality = parseInt(l, 10) || 40;
+
+    const randomIP = generateRandomIP();
+    const userAgent = randomUserAgent();
+  
   try {
     // Making the request with axios.get
     const axiosResponse = await axios.get(req.params.url, {
       headers: {
         ...pick(req.headers, ["cookie", "dnt", "referer"]),
-        "user-agent": "Bandwidth-Hero Compressor",
-        "x-forwarded-for": req.headers["x-forwarded-for"] || req.ip,
-        via: "1.1 bandwidth-hero",
+        'user-agent': userAgent,
+        'x-forwarded-for': randomIP,
+        'via': randomVia(),
       },
       responseType: 'stream', // To handle streaming data
       timeout: 10000,
