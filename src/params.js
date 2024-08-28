531599cc816d3af8344213dbf2782536d7aab49e
Rushs321 <fyggh566@tutamail.com>
2024-08-28 17:52:35 +0530

    Create params.js

---
+++ src/params.js
@@ -0,0 +1,14 @@
+"use strict";
+const DEFAULT_QUALITY = 40;
+
+async function params(req, res) {
+  let url = req.query.url;
+  if (!url) return res.send('1wert');
+
+  req.params.url = decodeURIComponent(url);
+  req.params.webp = !req.query.jpeg;
+  req.params.grayscale = req.query.bw != 0;
+  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
+}
+
+module.exports = params;
